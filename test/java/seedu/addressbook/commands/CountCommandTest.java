db34ecb2612513f0feccb0406fa1c4b2254627c7
Shaun Goh <shaun.goh@u.nus.edu>
2019-02-11 21:01:44 +0800

    Fix Person creation and exception handling in CountCommandTest
    CountCommand class now work correctly as a JUnit test for CountCommand

--- test/java/seedu/addressbook/commands/CountCommandTest.java
+++ test/java/seedu/addressbook/commands/CountCommandTest.java
@@ -3,49 +3,50 @@ package seedu.addressbook.commands;
 import org.junit.Test;
 import org.junit.Assert;
 import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.data.exception.IllegalValueException;
-import seedu.addressbook.data.person.Person;
-import seedu.addressbook.data.person.Name;
-import seedu.addressbook.data.person.Phone;
-import seedu.addressbook.data.person.Email;
-import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.*;
+
 import java.lang.String;
+import java.util.HashSet;
 
-class CountCommandTest {
+public class CountCommandTest {
 
-    public static final int NUM_PERSONS_IN_ADDRESSBOOK = 6;
-    public static final boolean DEFAULT_PRIVACY = false;
-    public static final String DEFAULT_PHONE = "123456789";
-    public static final String DEFAULT_EMAIL = "abc@123.com";
-    public static final String DEFAULT_ADDRESS = "10 Singapore Road, Singapore 999";
+    private static final int NUM_PERSONS_IN_ADDRESSBOOK = 6;
+    private static final boolean DEFAULT_PRIVACY = false;
+    private static final String DEFAULT_PHONE = "123456789";
+    private static final String DEFAULT_EMAIL = "abc@gmail.com";
+    private static final String DEFAULT_ADDRESS = "10 Singapore Road, Singapore 999";
 
     /**
      * Asserts that for a new addressbook with generated persons, Command Count counts the persons correctly
      * invalid persons data throws an IllegalValueException
      */
     @Test
-    void execute_newAddressBook_returnsCount() {
-        AddressBook book = new AddressBook();
+    public void execute_newAddressBook_returnsCount() {
+        AddressBook testBook = new AddressBook(); //
 
         try {
             for (int i = 0; i < NUM_PERSONS_IN_ADDRESSBOOK; i++) {
-                Person p = new Person(
-                        new Name(Integer.toString(i)),
-                        new Phone(DEFAULT_PHONE, DEFAULT_PRIVACY),
-                new Email(DEFAULT_EMAIL, DEFAULT_PRIVACY),
-                new Address(DEFAULT_ADDRESS, DEFAULT_PRIVACY), null
-                );
+                char alphabet = (char) i;
 
-                book.addPerson(p);
+                Person p = new Person(
+                        new Name("John Doe" + alphabet),
+                        new Phone(DEFAULT_PHONE + i, DEFAULT_PRIVACY),
+                        new Email(DEFAULT_EMAIL, DEFAULT_PRIVACY),
+                        new Address(DEFAULT_ADDRESS, DEFAULT_PRIVACY), new HashSet<>()
+                );
+                testBook.addPerson(p);
             }
-        } catch(IllegalValueException e) {
+
+        } catch(Exception e) {
+            System.out.println(e);
             Assert.fail();
         } finally {
             CountCommand testCommand = new CountCommand();
-            testCommand.setData(book,null); // "null" relevant persons is not under use
-            char outputChar = testCommand.execute().feedbackToUser.charAt(0);
+            testCommand.setData(testBook,null); // "null" relevant persons is not under use
 
-            Assert.assertEquals(NUM_PERSONS_IN_ADDRESSBOOK, String.valueOf(outputChar));
+            char testCountOutput = testCommand.execute().feedbackToUser.charAt(0);
+
+            Assert.assertEquals(String.valueOf(NUM_PERSONS_IN_ADDRESSBOOK), String.valueOf(testCountOutput));
         }
     }
 }
